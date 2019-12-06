325b1da18a3e563506148192083c7fcfa3da955f
Owen Jones <owen@owenjones.net>
2019-12-06 02:27:22 +0000

    Template SMTP driver - will flesh out later on today

--- reconciler/mail/smtp.py
+++ reconciler/mail/smtp.py
@@ -1,6 +1,9 @@
-from mail import Mail
+from reconciler.mail import Mail
 
 class SMTP(Mail) :
 
-    def __init__(self) :
+    def __init__(self, parameters) :
+        pass
+
+    def _send(self) :
         pass
