3e049a0b14bf43e14919e2223d7c0ea402d8b44d
Owen Jones <owen@owenjones.net>
2021-06-13 14:16:05 +0100

    updated both mail drivers to verify parameters, and formatted with black

--- reconciler/mail/mailgun.py
+++ reconciler/mail/mailgun.py
@@ -1,46 +1,46 @@
 import requests
+
 from reconciler.mail import MailDriver
+from reconciler.errors import ReconcilerMailerError
 
-class Mailgun (MailDriver) :
 
-    api      = None
-    key      = None
-    domain   = None
+class Mailgun(MailDriver):
 
-    def __init__(self, options) :
-        self.api      = options["api"]
-        self.key      = options["key"]
-        self.domain   = options["domain"]
+    api = None
+    key = None
+    domain = None
 
-        self.sender(options["from"])
+    def __init__(self, options):
+        self.api = options.get("api", None)
+        self.key = options.get("key", None)
+        self.domain = options.get("domain", None)
 
-    def endpoint(self, endpoint) :
-        url = "https://{}/v3/{}/{}".format(self.api, self.domain, endpoint)
-        return url
+        if not self.api or not self.key or not self.domain:
+            raise ReconcilerMailerError("Required Mailgun parameter is missing")
 
-    def _send(self) :
+    def endpoint(self, endpoint):
+        return f"https://{self.api}/v3/{self.domain}/{endpoint}"
+
+    def _send(self):
         auth = ("api", self.key)
 
-        files = [
-            ("attachment", (f, open(f, 'rb'))) for f in self.attachments
-        ]
+        files = [("attachment", (f, open(f, "rb"))) for f in self.attachments]
 
         data = {
-            "from"    : self.sendFrom,
-            "to"      : self.sendTo,
-            "cc"      : self.sendCC,
-            "bcc"     : self.sendBCC,
-            "subject" : self.subject,
-            "text"    : self.plainText
+            "from": self.sendFrom,
+            "to": self.sendTo,
+            "cc": self.sendCC,
+            "bcc": self.sendBCC,
+            "subject": self.subject,
+            "text": self.plainText,
         }
 
         send = requests.post(
-            self.endpoint("messages"),
-            auth=auth,
-            files=files,
-            data=data
+            self.endpoint("messages"), auth=auth, files=files, data=data
         )
 
-        if send.status_code != 200 :
-            error = "{}: {}".format(send.status_code, send.error) if send.error else send.status_code
-            raise HTTPError("Mailgun API Error - {}".format(error))
+        if send.status_code != 200:
+            error = (
+                f"{send.status_code}: {send.error}" if send.error else send.status_code
+            )
+            raise ReconcilerMailerError(f"Mailgun API Error - {error}")
--- reconciler/mail/smtp.py
+++ reconciler/mail/smtp.py
@@ -3,64 +3,67 @@ from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from reconciler.mail import MailDriver
 
-class SMTP (MailDriver) :
+from reconciler.mail import MailDriver
+from reconciler.errors import ReconcilerMailerError
+
+
+class SMTP(MailDriver):
 
     hostname = None
-    port     = None
+    port = None
     username = None
     password = None
-    ssl      = None
+    ssl = None
 
+    def __init__(self, parameters):
+        self.hostname = parameters.get("hostname", None)
+        self.port = parameters.get("port", 25)
+        self.username = parameters.get("username", None)
+        self.password = parameters.get("password", None)
+        self.ssl = parameters.get("ssl", False)
 
-    def __init__(self, parameters) :
-        self.hostname = parameters["hostname"]
-        self.port     = parameters["port"] if ("port" in parameters) else 25
-        self.username = parameters["username"]
-        self.password = parameters["password"]
+        if not self.hostname or not self.username or not self.password:
+            raise ReconcilerMailerError("Required SMTP parameter is missing")
 
-        self.ssl = parameters["ssl"] if ("ssl" in parameters) else False
-
-    def _parseAddressList(self, addresses) :
-        if isinstance(addresses, list) :
+    def _parseAddressList(self, addresses):
+        if isinstance(addresses, list):
             return ", ".join(addresses)
 
-        else :
+        else:
             return addresses
 
-    def _send(self) :
+    def _send(self):
         mail = MIMEMultipart()
-        mail["From"]    = self.sendFrom
+        mail["From"] = self.sendFrom
         mail["Subject"] = self.subject
 
-        if self.sendTo :
+        if self.sendTo:
             mail["To"] = self._parseAddressList(self.sendTo)
 
-        if self.sendCC :
+        if self.sendCC:
             mail["Cc"] = self._parseAddressList(self.sendCC)
 
-        if self.sendBCC :
+        if self.sendBCC:
             mail["Bcc"] = self._parseAddressList(self.sendBCC)
 
         mail.attach(MIMEText(self.plainText, "plain"))
 
-        if self.attachments :
-            for f in self.attachments :
-                with open(f, 'rb') as attachment :
+        if self.attachments:
+            for f in self.attachments:
+                with open(f, "rb") as attachment:
                     mime = MIMEBase("application", "octet-stream")
                     mime.set_payload(attachment.read())
                     encoders.encode_base64(mime)
 
                     mime.add_header(
-                        "Content-Disposition",
-                        "attachment; filename={}".format(f)
+                        "Content-Disposition", "attachment; filename={}".format(f)
                     )
 
                     mail.attach(mime)
 
-        with smtplib.SMTP(self.hostname, self.port) as smtp :
-            if self.ssl :
+        with smtplib.SMTP(self.hostname, self.port) as smtp:
+            if self.ssl:
                 smtp.starttls()
 
             smtp.login(self.username, self.password)
